02037a75755cab2a1cf386a206b2ca382e782c50
EhsanKehtari <ehsankehtari@gmail.com>
2023-12-30 00:06:47 +0330

    OX Crossover

--- Crossovers.py
+++ Crossovers.py
@@ -290,3 +290,52 @@ class Crossover:
             # To trigger the reproduction of the other offspring
             parent_random_number = 1 - parent_random_number
         return self.offspring_1, self.offspring_2
+
+    def ox(self) -> Tuple[ndarray, ndarray]:
+        """
+        Implement OX crossover.
+        :return: (tuple) two offsprings reproduced from given parents.
+        """
+        # Random positions to indicate the mapping section
+        random_positions = np.random.randint(
+            low=0,
+            high=len(self.offspring_1),
+            size=2
+        )
+        # Avoid duplicates
+        while random_positions[0] == random_positions[1]:
+            random_positions = np.random.randint(
+                low=0,
+                high=len(self.offspring_1),
+                size=2
+            )
+        # Lower and upper bound for substring selection
+        low = random_positions.min()
+        high = random_positions.max()
+        print(low + 1, high + 1)
+        # For parent1 selection at random
+        parent_random_number = random.random()
+        for i in range(2):
+            if parent_random_number <= 0.5:
+                parent_1, parent_2 = self.chromosome_1, self.chromosome_2
+                selected_offspring = self.offspring_1
+            else:
+                parent_2, parent_1 = self.chromosome_1, self.chromosome_2
+                selected_offspring = self.offspring_2
+            # Two lists to store genes in special positions and genes other than those in special positions
+            genes_in_special_positions = parent_1[low: high + 1]
+            genes_in_non_special_positions = list()
+            # Find genes other that those in special positions
+            for gene in parent_2:
+                if not (gene in genes_in_special_positions):
+                    genes_in_non_special_positions.append(gene)
+            # Offspring reproduction logic
+            for gene_num in range(len(selected_offspring)):
+                if gene_num in range(low, high + 1):
+                    selected_offspring[gene_num] = parent_1[gene_num]
+                else:
+                    selected_offspring[gene_num] = genes_in_non_special_positions.pop(0)
+            # To trigger the reproduction of the other offspring
+            parent_random_number = 1 - parent_random_number
+        return self.offspring_1, self.offspring_2
+
